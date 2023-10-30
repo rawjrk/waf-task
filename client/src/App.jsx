43196a8c331eb39eaa4e8e4507ef375ea723f5bd
RawJrk <rodjer7777@gmail.com>
2023-10-30 20:50:54 +0200

    setup react with vite

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>SnakeN</h1>
+      <ul>
+        <li>Start</li>
+        <li>Scores</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
