30a5da596aa6f8c9a87aea4fa9abfa36403ddc6a
RawJrk <rodjer7777@gmail.com>
2023-11-02 17:27:20 +0200

    added basic routing and queries

--- server/index.js
+++ server/index.js
@@ -4,6 +4,41 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
+const { query } = require('./db')
+
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  query(
+    `SELECT * FROM score
+    ORDER BY points DESC
+    LIMIT 20`,
+  )
+    .then((data) => {
+      res.status(200).send(data)
+    })
+    .catch((error) => {
+      res.status(500).send({ status: 500, message: error.message })
+      console.error(error)
+    })
+})
+
+app.post('/new', (req, res) => {
+  const { name, points } = req.body
+
+  query(
+    `INSERT INTO score (name, points, dt)
+    VALUES ($1, $2, NOW() )`,
+    [name, points],
+  )
+    .then((data) => {
+      res.status(200).send({ status: 200, message: 'success!' })
+    })
+    .catch((error) => {
+      res.status(500).send({ status: 500, message: error.message })
+      console.error(error)
+    })
+})
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
