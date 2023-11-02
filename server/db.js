fb72578e0fc432294cdbed2fe50ddbba1d34874e
RawJrk <rodjer7777@gmail.com>
2023-11-02 17:26:21 +0200

    setup server and db connection

---
+++ server/db.js
@@ -0,0 +1,12 @@
+const pgp = require('pg-promise')()
+
+const username = process.env.POSTGRES_USER
+const password = process.env.POSTGRES_PASSWORD
+const host = process.env.POSTGRES_HOST
+const port = process.env.POSTGRES_PORT
+
+const db = pgp(`postgress://${username}:${password}@${host}:${port}/snaken`)
+
+module.exports = {
+  query: (text, values) => db.query(text, values),
+}
---
+++ server/index.js
@@ -0,0 +1,10 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
