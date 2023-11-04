ad8d410b27a622d4fad806c5244c8dd6501bb2a3
RawJrk <rodjer7777@gmail.com>
2023-11-04 11:03:25 +0200

    preparing for deploy

--- server/db.js
+++ server/db.js
@@ -5,7 +5,9 @@ const password = process.env.POSTGRES_PASSWORD
 const host = process.env.POSTGRES_HOST
 const port = process.env.POSTGRES_PORT
 
-const db = pgp(`postgress://${username}:${password}@${host}:${port}/snaken`)
+const db = pgp(
+  `postgress://${username}:${password}@${host}:${port}/snaken?ssl=true`,
+)
 
 module.exports = {
   query: (text, values) => db.query(text, values),
--- server/index.js
+++ server/index.js
@@ -3,13 +3,19 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const app = express()
+const path = require('node:path')
 const PORT = process.env.PORT || 5000
 
 const apiRoutes = require('./api/routes')
 
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, '../client/dist')))
 
 app.use('/api', apiRoutes)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname), '../client/dist/index.html')
+})
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
