c94db8189bad2c0ec9dd091adc887de5e9d65712
RawJrk <rodjer7777@gmail.com>
2023-11-05 10:42:20 +0200

    postgres uri stored fully as env variable

--- server/db.js
+++ server/db.js
@@ -1,13 +1,6 @@
 const pgp = require('pg-promise')()
 
-const username = process.env.POSTGRES_USER
-const password = process.env.POSTGRES_PASSWORD
-const host = process.env.POSTGRES_HOST
-const port = process.env.POSTGRES_PORT
-
-const db = pgp(
-  `postgress://${username}:${password}@${host}:${port}/snaken?ssl=true`,
-)
+const db = pgp(process.env.POSTGRES_URI)
 
 module.exports = {
   query: (text, values) => db.query(text, values),
