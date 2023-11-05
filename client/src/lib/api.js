5ca54fb02d295182169e68069135f3d3d8040c53
RawJrk <rodjer7777@gmail.com>
2023-11-05 11:05:18 +0200

    reformatted api response

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,9 +2,13 @@ export const getScoreList = async (options = {}) => {
   const { offset = 0, limit = 5 } = options
 
   try {
-    const data = await (
-      await fetch(`/api?limit=${limit}&offset=${offset}`)
-    ).json()
+    const response = await fetch(`/api?limit=${limit}&offset=${offset}`)
+    const json = await response.json()
+
+    const { status, message, data } = json
+    if (status !== 200) {
+      throw Error(message)
+    }
     return data
   } catch (error) {
     console.error(error)
@@ -14,11 +18,17 @@ export const getScoreList = async (options = {}) => {
 export const addScore = async (name, points, feedOption) => {
   try {
     const data = { name, points, feedOption }
-    await fetch('/api/new', {
+    const response = await fetch('/api/new', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
     })
+    const json = await response.json()
+
+    const { status, message } = json
+    if (status !== 200) {
+      throw Error(message)
+    }
   } catch (error) {
     console.error(error)
   }
--- server/api/controllers.js
+++ server/api/controllers.js
@@ -3,23 +3,28 @@ const { query } = require('../db')
 module.exports.index = async (req, res) => {
   try {
     const { limit = 5, offset = 0 } = req.query
-    const data = await query(
+
+    const queryResult = await query(
       `SELECT * FROM score ORDER BY points
       DESC LIMIT $1 OFFSET $2`,
       [limit, offset],
     )
 
-    const result = data.map((row, i) => ({
+    const data = queryResult.map((row, i) => ({
       position: i + 1 + parseInt(offset),
       name: row.name,
       points: row.points,
       feedOption: row['feed_option'],
     }))
 
-    res.status(200).send(result)
+    res.status(200).send({
+      status: 200,
+      message: 'success',
+      data,
+    })
   } catch (error) {
-    res.status(500).send({ status: 500, message: error.message })
     console.error(error)
+    res.status(500).send({ status: 500, message: error.message })
   }
 }
 
@@ -31,9 +36,13 @@ module.exports.create = async (req, res) => {
       VALUES ($1, $2, $3, NOW() )`,
       [name, points, feedOption],
     )
-    res.status(200).send({ status: 200, message: 'success!' })
+
+    res.status(200).send({
+      status: 200,
+      message: 'success',
+    })
   } catch (error) {
-    res.status(500).send({ status: 500, message: error.message })
     console.error(error)
+    res.status(500).send({ status: 500, message: error.message })
   }
 }
